b25d991e9bed6f5af57391387e29ddc5dc1c6b09
lazarus <lazarusr.garcia@gmail.com>
2023-08-25 23:02:55 -0500

    looping

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+function writeCards(message,event) {
+    const messageArray = []
+    for (let i = 0; i < message.length; i++) {
+        messageArray.push(`Thank you, ${message[i]}, for the wonderful ${event} gift!`)
+    }
+    return messageArray;
+}
+function countDown(intNum) {
+    while (intNum >= 0) {
+      console.log(intNum);
+      intNum--;
+    }
+    return intNum;
+  }
+  countDown(10);
